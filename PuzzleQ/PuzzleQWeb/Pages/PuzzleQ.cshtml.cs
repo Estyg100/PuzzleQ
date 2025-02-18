d5f805d6ab4480c7a314f82a8e4a8ef06bd9bd7c
estyg100 <estygrunwald1@gmail.com>
2025-02-18 17:54:06 -0500

    PuzzleQWeb

---
+++ PuzzleQ/PuzzleQWeb/Pages/PuzzleQ.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PuzzleQWeb.Pages
+{
+    public class PuzzleQModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
