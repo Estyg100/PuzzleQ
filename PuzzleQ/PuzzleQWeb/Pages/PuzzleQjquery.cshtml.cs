5eaf4388d4e8fd4a3bc2a527d2fdba4194133c5a
estyg100 <estygrunwald1@gmail.com>
2025-02-20 00:06:07 -0500

    jQuery

---
+++ PuzzleQ/PuzzleQWeb/Pages/PuzzleQjquery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PuzzleQWeb.Pages
+{
+    public class PuzzleQjqueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
